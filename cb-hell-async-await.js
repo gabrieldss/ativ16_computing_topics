64bb68a5df4b889d546db3a3c51d5bfd748d263b
gabrieldss <33532210+gabrieldss@users.noreply.github.com>
2020-08-26 23:57:13 -0300

    add codes

---
+++ cb-hell-async-await.js
@@ -0,0 +1,8 @@
+const fsp = require('fs').promises;
+
+(async () => {
+    const fileName2 = await fsp.readFile('f1.txt', 'utf-8');
+    const fileName3 = await fsp.readFile(fileName2, 'utf-8');
+    const fileContent = await fsp.readFile(fileName3, 'utf-8');
+    console.log('Conteudo: ', fileContent);
+})();
